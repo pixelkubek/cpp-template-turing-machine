539d4e4fad69bb339186081c71f0e64f458f94d1
pixelkubek <150136485+pixelkubek@users.noreply.github.com>
2025-10-06 14:25:44 +0200

    Partially implement advance

--- machine.h
+++ machine.h
@@ -17,22 +17,37 @@ struct direction {
 };
 
 template <typename State, typename TapeContent>
-struct next {};
+struct next_state {};
 
-template <typename New, typename Instruction>
-struct instruction {};
+template <typename State, typename TapeContent>
+struct chosen_symbol {};
 
-template <typename New, typename Instruction>
-using program = std::type_identity<instruction<New, Instruction>>;
 
 template <>
-struct next<state::start, blank> : program<state::accept, blank> {};
+struct next_state<state::start, blank> : std::type_identity<state::accept> {};
 
-static_assert(next<state::start, blank>::type)
+template <>
+struct chosen_symbol<state::start, blank> : std::type_identity<blank> {};
 
+
+template <typename State, typename TapeContent>
+using next_state_t = typename next_state<State, TapeContent>::type;
+
+template <typename State, typename TapeContent>
+using chosen_symbol_t = typename chosen_symbol<State, TapeContent>::type;
 
 template <typename State, typename Tape>
 struct machine {};
+
+template <typename T>
+struct advance {};
+
+template <typename State, typename Tape>
+struct advance<machine<State, Tape>> : std::type_identity<machine<
+    next_state_t<State, header_read_t<Tape>>,
+    void
+>> {};
+
 
 using machine_instance = machine<state::start, new_tape_t>;
 
